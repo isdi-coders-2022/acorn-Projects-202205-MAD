526597e55eeee5d46418d13793dc29714201c2fe
Rodrigo Calvo <rodrigo.calvo.blazquez@gmail.com>
2022-06-06 15:25:45 +0200

    Update store, change method names

--- src/services/match.http.store.ts
+++ src/services/match.http.store.ts
@@ -3,13 +3,13 @@ import { iMatch } from '../models/interface';
 export class MatchHttpStore {
     constructor(public url: string) {}
 
-    getData(id: string): Promise<iMatch> {
+    getMatch(id: string): Promise<iMatch> {
         return fetch(this.url + id).then((response) => response.json());
     }
-    getAllData(): Promise<Array<iMatch>> {
+    getAllMatch(): Promise<Array<iMatch>> {
         return fetch(this.url).then((response) => response.json());
     }
-    setData(match: iMatch): Promise<iMatch> {
+    setMatch(match: iMatch): Promise<iMatch> {
         return fetch(this.url, {
             method: 'POST',
             body: JSON.stringify(match),
@@ -18,7 +18,7 @@ export class MatchHttpStore {
             },
         }).then((response) => response.json());
     }
-    updateData(match: Partial<iMatch>): Promise<iMatch> {
+    updateMatch(match: Partial<iMatch>): Promise<iMatch> {
         return fetch(this.url + match.id, {
             method: 'PATCH',
             body: JSON.stringify(match),
@@ -28,7 +28,7 @@ export class MatchHttpStore {
         }).then((response) => response.json());
     }
 
-    deleteData(match: iMatch): Promise<iMatch> {
+    deleteMatch(match: iMatch): Promise<iMatch> {
         return fetch(this.url + match.id, {
             method: 'DELETE',
         }).then((response) => response.json());
