3bcaac47d37935db3e0db749bf2e07e752d45434
FranLobo <floboflores@gmail.com>
2022-06-06 18:07:38 +0200

    ADD header

---
+++ src/components/header.tsx
@@ -0,0 +1,16 @@
+
+
+
+
+export function Header(){
+
+    return(
+        <header>    
+         <img src="./img/logo.png"/>
+                        
+           {/* <Navigation/>  */}
+
+        </header>
+ 
+    )
+};
