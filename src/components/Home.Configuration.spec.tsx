f22f69e3d658321523e21c85a884fcb8916d1c05
Rodrigo Calvo <rodrigo.calvo.blazquez@gmail.com>
2022-06-07 12:03:17 +0200

    Add configuration component test

---
+++ src/components/Home.Configuration.spec.tsx
@@ -0,0 +1,12 @@
+import { render, screen } from '@testing-library/react';
+import { Configuration } from './Home.Configuration';
+
+describe('Given Home > Info component', () => {
+    describe('When calling it', () => {
+        test('It should render', () => {
+            render(<Configuration />);
+            const labelInput = screen.getByRole('combobox');
+            expect(labelInput).toBeInTheDocument();
+        });
+    });
+});
