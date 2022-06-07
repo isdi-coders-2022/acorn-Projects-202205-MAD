3bc56af6025c9ad922bf8ea2f2e34ef82025fb1b
Rodrigo Calvo <rodrigo.calvo.blazquez@gmail.com>
2022-06-07 12:30:08 +0200

    Add navigation element test

---
+++ src/components/All.Navigation.spec.tsx
@@ -0,0 +1,12 @@
+import { render, screen } from '@testing-library/react';
+import { Navigation } from './All.Navigation';
+
+describe('Given Home > Info component', () => {
+    describe('When calling it', () => {
+        test('It should render', () => {
+            render(<Navigation />);
+            const labelInput = screen.getByRole('button');
+            expect(labelInput).toBeInTheDocument();
+        });
+    });
+});
