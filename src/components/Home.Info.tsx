f3fef3a74104d79c1a7f1d26dfd082a130b0602f
Rodrigo Calvo <rodrigo.calvo.blazquez@gmail.com>
2022-06-07 09:37:19 +0200

    Add home > info component

--- src/components/Home.Info.tsx
+++ src/components/Home.Info.tsx
@@ -1,4 +1,19 @@
 export function Info() {
-    const template = <></>;
+    const template = (
+        <section className="info">
+            <ul className="info__images">
+                <li>
+                    <img src="" alt="carousel1" />
+                </li>
+                <li>
+                    <img src="" alt="carousel2" />
+                </li>
+                <li>
+                    <img src="" alt="carousel3" />
+                </li>
+            </ul>
+            <p className="info__description"></p>
+        </section>
+    );
     return template;
 }
