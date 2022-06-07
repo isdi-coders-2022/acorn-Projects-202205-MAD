1b80ed1c67a9d3bcbb1fd1a9acf1316329cb3fcb
Rodrigo Calvo <rodrigo.calvo.blazquez@gmail.com>
2022-06-07 09:37:39 +0200

    Add results component to home page

--- src/components/Home.Results.tsx
+++ src/components/Home.Results.tsx
@@ -1,4 +1,28 @@
-export function Results() {
-    const template = <></>;
+import { iFilm } from '../models/interface';
+
+export function Results({
+    currentWeather,
+    resultsArray,
+}: {
+    currentWeather: string;
+    resultsArray: Array<iFilm>;
+}) {
+    const template = (
+        <section className="results">
+            <p>Recomendaciones para: {currentWeather}</p>
+            <ul>
+                {resultsArray.map((result) => (
+                    <li key={result.id}>
+                        <figure>
+                            <img src={result.image} alt="result poster" />
+                            <figcaption>
+                                {result.title} {result.year}
+                            </figcaption>
+                        </figure>
+                    </li>
+                ))}
+            </ul>
+        </section>
+    );
     return template;
 }
