3d9af8fc535ea11567031a44f2e24de0a5845be6
Rodrigo Calvo <rodrigo.calvo.blazquez@gmail.com>
2022-06-07 09:37:54 +0200

    Add home>configuration component

--- src/components/Home.Configuration.tsx
+++ src/components/Home.Configuration.tsx
@@ -1,4 +1,19 @@
 export function Configuration() {
-    const template = <></>;
+    const template = (
+        <section className="config">
+            <select
+                className="config__selector"
+                name="weather-config"
+                id="weather-config"
+            >
+                <option value="">--Selecciona un clima--</option>
+                <option value="soleado">Soleado</option>
+                <option value="caluroso">Caluroso</option>
+                <option value="lluvia">Lluvia</option>
+                <option value="frio y nublado">Frío y nublado</option>
+                <option value="nieve">Nieve</option>
+            </select>
+        </section>
+    );
     return template;
 }
