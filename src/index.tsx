6b075ef4c5f88cd1740f3cbbcdfd513b93dd8552
Adrian Calero <acalerobarrero@gmail.com>
2022-06-08 17:05:23 +0200

    Adding matchContextProvider

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import { Auth0Provider } from '@auth0/auth0-react';
+import { MatchContextProvider } from './contexts/match.provider';
 
 const root = ReactDOM.createRoot(
     document.getElementById('root') as HTMLElement
@@ -15,9 +16,11 @@ root.render(
             clientId="WkimkESzOTGKCBuBVUkGgyRcO3ZjTmmF"
             redirectUri={window.location.origin}
         >
-            <BrowserRouter>
-                <App />
-            </BrowserRouter>
+            <MatchContextProvider>
+                <BrowserRouter>
+                    <App />
+                </BrowserRouter>
+            </MatchContextProvider>
         </Auth0Provider>
     </React.StrictMode>
 );
