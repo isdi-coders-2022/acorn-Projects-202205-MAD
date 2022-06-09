13ca738e1498c14f1c442fa1488f92ed5c1134a4
Rodrigo Calvo <rodrigo.calvo.blazquez@gmail.com>
2022-06-09 16:10:17 +0200

    Add integration home components

--- src/components/Home.Configuration.tsx
+++ src/components/Home.Configuration.tsx
@@ -1,4 +1,98 @@
+import { SyntheticEvent, useEffect, useState } from 'react';
+import { premadeMatches } from '../data/premade-matches';
+import { complexWeathers, weatherMatchers } from '../data/forecast-conversion';
+import { iFilm, iWeather, spanishWeather } from '../models/interface';
+import { WeatherHttpStore } from '../services/weather.http.store';
+import { FilmHttpStore } from '../services/films.http.store';
+import { Results } from './Home.Results';
+
 export function Configuration() {
+    const [configData, setConfigData] = useState({
+        location: '',
+        weather: 'Soleado',
+    });
+    const initialValue: iWeather = {
+        location: {
+            name: '',
+            region: '',
+            country: '',
+        },
+        current: {
+            temp_c: 25,
+            condition: {
+                text: 'Sunny',
+            },
+        },
+    };
+    const [currentWeather, setCurrentWeather] = useState(initialValue);
+    const initialResultsValue: Array<iFilm> = [];
+    const [currentResults, setCurrentResults] = useState(initialResultsValue);
+
+    useEffect(() => {}, []);
+
+    const complexWeather = currentWeather.current.condition
+        .text as complexWeathers;
+    const simpleWeather = weatherMatchers[complexWeather] as spanishWeather;
+
+    function handleButtonClick(ev: SyntheticEvent) {
+        ev.preventDefault();
+        if (configData.location) {
+            new WeatherHttpStore()
+                .getWeather(configData.location)
+                .then((weatherGetted) => {
+                    setCurrentWeather(weatherGetted);
+                    const complexWeather = currentWeather.current.condition
+                        .text as complexWeathers;
+                    let simpleWeather = weatherMatchers[
+                        complexWeather
+                    ] as spanishWeather;
+                    if (
+                        simpleWeather === 'Soleado' &&
+                        currentWeather.current.temp_c > 29
+                    ) {
+                        simpleWeather = 'Caluroso';
+                    }
+                    const films: Array<number> = premadeMatches[simpleWeather];
+
+                    let tresFilms = [];
+                    for (let i = 0; i < 3; i++) {
+                        const random = Math.floor(Math.random() * films.length);
+                        tresFilms.push(films.splice(random, 1)[0]);
+                    }
+                    const filmApi = new FilmHttpStore();
+                    let promises: Array<Promise<iFilm>> = tresFilms.map((id) =>
+                        filmApi.getFilm(id)
+                    );
+                    Promise.all(promises).then((threeFilms) =>
+                        setCurrentResults(threeFilms)
+                    );
+                });
+        } else if (configData.weather) {
+            let simpleWeather = configData.weather as spanishWeather;
+            const films: Array<number> = premadeMatches[simpleWeather];
+
+            let tresFilms = [];
+            for (let i = 0; i < 3; i++) {
+                const random = Math.floor(Math.random() * films.length);
+                tresFilms.push(films.splice(random, 1)[0]);
+            }
+            const filmApi = new FilmHttpStore();
+            let promises: Array<Promise<iFilm>> = tresFilms.map((id) =>
+                filmApi.getFilm(id)
+            );
+            Promise.all(promises).then((threeFilms) =>
+                setCurrentResults(threeFilms)
+            );
+        }
+    }
+    function handleTextChange(ev: SyntheticEvent) {
+        const eventTarget = ev.target as HTMLFormElement;
+        setConfigData({ ...configData, location: eventTarget.value });
+    }
+    function handleSelectChange(ev: SyntheticEvent) {
+        const eventTarget = ev.target as HTMLFormElement;
+        setConfigData({ ...configData, weather: eventTarget.value });
+    }
     const template = (
         <>
             {' '}
@@ -10,20 +104,24 @@ export function Configuration() {
             <div className="container">
                 <div className="form-group">
                     <input
+                        onChange={(ev) => handleTextChange(ev)}
                         type="text"
                         className="email-bt"
                         placeholder="Ubicación               🧭"
                         name="Name"
+                        value={configData.location}
                     />
                 </div>
-
                 <div className="form-group">
-                    <select name="select">
-                        <option value="value1">Caluroso</option>
-                        <option value="value2">Soleado</option>
-                        <option value="value3">Lluvioso</option>
-                        <option value="value4">Frio y Nublado</option>
-                        <option value="value5">Nieve</option>
+                    <select
+                        name="select"
+                        onChange={(ev) => handleSelectChange(ev)}
+                    >
+                        <option value="Caluroso">Caluroso</option>
+                        <option value="Soleado">Soleado</option>
+                        <option value="Lluvia">Lluvioso</option>
+                        <option value="Frio y nublado">Frio y Nublado</option>
+                        <option value="Nieve">Nieve</option>
                     </select>
                 </div>
             </div>
@@ -36,10 +134,17 @@ export function Configuration() {
                 mente¨
             </p>
             <div className="send_btn">
-                <button type="button" className="main_bt">
-                    <a href="#">Enviar</a>
+                <button
+                    onClick={(ev) => handleButtonClick(ev)}
+                    className="main_bt"
+                >
+                    Enviar
                 </button>
             </div>
+            <Results
+                currentWeather={simpleWeather}
+                resultsArray={currentResults}
+            />
         </>
     );
     return template;
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,65 +1,12 @@
-import { useEffect, useState } from 'react';
-import { premadeMatches } from '../data/premade-matches';
-import { complexWeathers, weatherMatchers } from '../data/forecast-conversion';
-import { iFilm, iWeather, spanishWeather } from '../models/interface';
-import { WeatherHttpStore } from '../services/weather.http.store';
-import { FilmHttpStore } from '../services/films.http.store';
 import './home.css';
 import { Info } from '../components/Home.Info';
 import { Configuration } from '../components/Home.Configuration';
 export function Home() {
-    const initialValue: iWeather = {
-        location: {
-            name: '',
-            region: '',
-            country: '',
-        },
-        current: {
-            temp_c: 25,
-            condition: {
-                text: 'Sunny',
-            },
-        },
-    };
-    const [currentWeather, setCurrentWeather] = useState(initialValue);
-    const initialResultsValue: Array<iFilm> = [];
-    const [currentResults, setCurrentResults] = useState(initialResultsValue);
-    useEffect(() => {
-        new WeatherHttpStore().getWeather('Londres').then((weatherGetted) => {
-            setCurrentWeather(weatherGetted);
-            const complexWeather = currentWeather.current.condition
-                .text as complexWeathers;
-            let simpleWeather = weatherMatchers[
-                complexWeather
-            ] as spanishWeather;
-            if (
-                simpleWeather === 'Soleado' &&
-                currentWeather.current.temp_c > 29
-            ) {
-                simpleWeather = 'Caluroso';
-            }
-            const films: Array<number> = premadeMatches[simpleWeather];
-
-            let tresFilms = [];
-            for (let i = 0; i < 3; i++) {
-                const random = Math.floor(Math.random() * films.length);
-                tresFilms.push(films.splice(random, 1)[0]);
-            }
-            const filmApi = new FilmHttpStore();
-            let promises: Array<Promise<iFilm>> = tresFilms.map((id) =>
-                filmApi.getFilm(id)
-            );
-            Promise.all(promises).then((threeFilms) =>
-                setCurrentResults(threeFilms)
-            );
-        });
-    }, []);
-
     const template = (
         <>
             <Info />
             <Configuration />
-            {currentResults.map((film) => film.title)}
+            {/* {currentResults.map((film) => film.title)} AQUI VA RESULTS */}
         </>
     );
     return template;
