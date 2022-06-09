6e488ed1114cac32c1d3a57d6222bab289b55df4
Rodrigo Calvo <rodrigo.calvo.blazquez@gmail.com>
2022-06-09 20:39:18 +0200

    Add filmdetails and homeconfig tests

--- src/components/Details.FilmDetails.spec.tsx
+++ src/components/Details.FilmDetails.spec.tsx
@@ -1,14 +1,34 @@
 import { render, screen } from '@testing-library/react';
-import { useParams } from 'react-router-dom';
 import { FilmDetails } from './Details.FilmDetails';
+import { FilmHttpStore } from '../services/films.http.store';
+import { iFilm, iVideo } from '../models/interface';
 
-describe('Given Home > Info component', () => {
+jest.mock('../services/films.http.store');
+const film1: iFilm = {
+    id: 0,
+    overview: 'test',
+    poster_path: 'test',
+    release_date: 'test',
+    title: 'test title',
+    vote_average: 0,
+};
+const video1: iVideo = {
+    id: 0,
+    results: [{ key: 'test', type: 'test' }],
+};
+
+describe('Given FilmDetails component', () => {
+    beforeEach(() => {
+        FilmHttpStore.prototype.getFilm = jest.fn().mockResolvedValue(film1);
+        FilmHttpStore.prototype.getVideo = jest.fn().mockResolvedValue(video1);
+    });
     describe('When calling it', () => {
-        test('It should render', () => {
-            const { id } = useParams();
-            const idFilm = id as string;
-            render(<FilmDetails idFilm={idFilm} />);
-            const labelInput = screen.getByText(/Sinopsis/i);
+        test('It should ask data to the api and render', async () => {
+            const id = 0;
+
+            render(<FilmDetails idFilm={id.toString()} />);
+
+            const labelInput = await screen.findByText(/test title/i);
             expect(labelInput).toBeInTheDocument();
         });
     });
